26c6b2534c641e7c022888338b8ce2f07843a6a8
pipopotamasu <ec0156hx@gmail.com>
2021-12-19 23:38:09 +0900

    routes setting

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,17 @@
 import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from 'App';
 
 describe('App', () => {
-  it('count up', () => {
-    const { getByText } = render(<App />);
-    const counter = getByText('count is: 0');
-    fireEvent.click(counter);
-    getByText('count is: 1');
+  it('count up', async () => {
+    const { getByText, findByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    await findByText('Welcome to the homepage!');
+    const aboutLink = getByText('About');
+    fireEvent.click(aboutLink);
+    await findByText('Who are we?');
   });
 });
