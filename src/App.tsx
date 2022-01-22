ea01460ae4b90006feea608d988097084e810c6a
pipopotamasu <ec0156hx@gmail.com>
2022-01-22 14:38:48 +0900

    add style header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { sum } from '@/utils/sample';
-import logo from '@/assets/logo.svg';
+import reactLogo from '@/assets/react-logo.svg';
+import viteLogo from '@/assets/vite-logo.svg';
 
 const Home = lazy(() => import('./Home'));
 const About = lazy(() => import('./About'));
 
 function App() {
   return (
-    <div>
-      <header>
-        <img src={logo} alt="logo" />
-        <p>Hello Vite + React!</p>
-        <button onClick={() => alert(sum(1, 2))}>click me</button>
-        <Suspense fallback="loading...">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-          </Routes>
-        </Suspense>
+    <div className="h-full py-16">
+      <header className="flex flex-col items-center">
+        <h1 className="font-bold text-4xl mb-4">Hello vite-react!</h1>
+        <div className="flex justify-center pl-8">
+          <img src={viteLogo} alt="vite-logo" className="w-40" />
+          <img src={reactLogo} alt="react-logo" className="w-72" />
+        </div>
       </header>
+      <main className="py-8 flex justify-center">
+        <div className="w-2/5">
+          <button onClick={() => alert(sum(1, 2))}>click me</button>
+          <Suspense fallback="loading...">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+            </Routes>
+          </Suspense>
+        </div>
+      </main>
     </div>
   );
 }
