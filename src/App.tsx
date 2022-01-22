d168d77cc88b290396d543b769c3a97ae7a7fcfa
pipopotamasu <ec0156hx@gmail.com>
2022-01-22 14:10:27 +0900

    rm css

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,17 @@ import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { sum } from '@/utils/sample';
 import logo from '@/assets/logo.svg';
-import '@/assets/App.css';
 
 const Home = lazy(() => import('./Home'));
 const About = lazy(() => import('./About'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <img src={logo} alt="logo" />
         <p>Hello Vite + React!</p>
-        <button onClick={() => alert(sum(1, 2))} className="underline">
-          click me
-        </button>
+        <button onClick={() => alert(sum(1, 2))}>click me</button>
         <Suspense fallback="loading...">
           <Routes>
             <Route path="/" element={<Home />} />
