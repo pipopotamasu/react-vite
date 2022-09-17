d38d4ed32be824892550860b74b4fb3ffd0aaf0d
pipopotamasu <ec0156hx@gmail.com>
2022-09-17 14:44:59 -0700

    update libs

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import 'tailwindcss/tailwind.css';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
