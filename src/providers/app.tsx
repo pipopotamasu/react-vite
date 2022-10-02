ee25eb8f75607d20bed5ff1058211d311f8c1f70
pipopotamasu <ec0156hx@gmail.com>
2022-10-02 00:01:22 -0700

    error boundary (#32)

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,9 +1,24 @@
 import { Suspense, type ReactNode } from 'react';
 import { QueryClientProvider } from 'react-query';
 import { ToastContainer } from 'react-toastify';
+import { ErrorBoundary } from 'react-error-boundary';
 import { ReactLocation, Router } from '@tanstack/react-location';
 import { queryClient } from '@/lib/react-query';
 import { routes } from '@/routes';
+
+const ErrorFallback = () => {
+  return (
+    <div
+      className="bg-gray-700 text-red-500 w-screen h-screen flex flex-col justify-center items-center"
+      role="alert"
+    >
+      <h2 className="text-lg font-semibold">Ooops, something went wrong :( </h2>
+      <button className="mt-4" onClick={() => window.location.assign(window.location.origin)}>
+        Refresh
+      </button>
+    </div>
+  );
+};
 
 const location = new ReactLocation();
 
@@ -14,11 +29,13 @@ type AppProviderProps = {
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <Suspense fallback={<>loading...</>}>
-      <QueryClientProvider client={queryClient}>
-        <Router location={location} routes={routes}>
-          {children}
-        </Router>
-      </QueryClientProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <QueryClientProvider client={queryClient}>
+          <Router location={location} routes={routes}>
+            {children}
+          </Router>
+        </QueryClientProvider>
+      </ErrorBoundary>
       <ToastContainer />
     </Suspense>
   );
