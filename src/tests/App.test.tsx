88e0f9991a95dea8244b243db93e07855d6cf5e7
pipopotamasu <ec0156hx@gmail.com>
2022-10-02 15:26:56 -0700

    fix test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -5,6 +5,5 @@ describe('App', () => {
   it('renders', async () => {
     const { findByText } = render(<App />);
     await findByText('Hello vite-react!');
-    await findByText('root');
   });
 });
