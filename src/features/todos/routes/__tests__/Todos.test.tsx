5d1a289a77f4c636d333d52d04e7cb1a4b135676
pipopotamasu <ec0156hx@gmail.com>
2022-12-17 21:55:52 -0800

    update libs

--- src/features/todos/routes/__tests__/Todos.test.tsx
+++ src/features/todos/routes/__tests__/Todos.test.tsx
@@ -1,11 +1,22 @@
 import { renderApp } from '@/test/testUtils';
+import { server } from '@/mocks/server';
+import { rest } from 'msw';
 
 describe('Todos', () => {
   it('renders', async () => {
+    server.use(
+      rest.get('/todos', async (_, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            todos: [{ id: 1, title: 'todo11', done: false }],
+          })
+        );
+      })
+    );
+
     const { findByText } = renderApp({ route: '/todos' });
     await findByText('Todo App');
-    await findByText('todo1');
-    await findByText('todo2');
-    await findByText('todo3');
+    await findByText('todo11');
   });
 });
