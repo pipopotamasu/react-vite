90d8d7457b8741fe9d1d71b7743d317a053d52dc
pipopotamasu <ec0156hx@gmail.com>
2023-10-14 22:29:32 -0400

    wip

--- eslint/index.js
+++ eslint/index.js
@@ -1,37 +1,42 @@
-module.exports = {
-  rules: {
-    'no-template-literals': {
-      meta: {
-        type: "problem",
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("@typescript-eslint/utils");
+const rule = utils_1.ESLintUtils.RuleCreator.withoutDocs({
+    defaultOptions: [],
+    meta: {
+        type: 'problem',
         docs: {
-          description: "test"
+            description: 'test',
         },
-        fixable: "code",
-        schema: []
+        messages: { missingAnyType: 'Missing any type' },
+        fixable: 'code',
+        schema: [],
     },
-      create: function (context) {
+    create: function (context) {
         console.log('context', context);
         return {
-          FunctionDeclaration(node) {
-            console.log('function declaration', node);
-
-            if (node.params) {
-              node.params.forEach((param) => {
-                console.log('param', param);
-                if (!param.typeAnnotation) {
-                  context.report({
-                    node,
-                    message: "Missing any type",
-                    fix(fixer) {
-                        return fixer.insertTextAfter(param, ": any");
-                    }
-                  });
+            FunctionDeclaration(node) {
+                console.log('function declaration', node);
+                if (node.params.length > 0) {
+                    node.params.forEach((param) => {
+                        console.log('param', param);
+                        if (!param['typeAnnotation']) {
+                            context.report({
+                                node,
+                                messageId: 'missingAnyType',
+                                fix(fixer) {
+                                    return fixer.insertTextAfter(param, ': any');
+                                },
+                            });
+                        }
+                    });
                 }
-              })
-            }
-          }
+            },
         };
-      },
     },
-  },
+});
+module.exports = {
+    rules: {
+        'no-template-literals': rule,
+    },
 };
---
+++ eslint/index.ts
@@ -0,0 +1,43 @@
+import { ESLintUtils } from '@typescript-eslint/utils';
+
+const rule = ESLintUtils.RuleCreator.withoutDocs({
+  defaultOptions: [],
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'test',
+    },
+    messages: { missingAnyType: 'Missing any type' },
+    fixable: 'code',
+    schema: [],
+  },
+  create: function (context) {
+    console.log('context', context);
+    return {
+      FunctionDeclaration(node) {
+        console.log('function declaration', node);
+
+        if (node.params.length > 0) {
+          node.params.forEach((param) => {
+            console.log('param', param);
+            if (!param['typeAnnotation']) {
+              context.report({
+                node,
+                messageId: 'missingAnyType',
+                fix(fixer) {
+                  return fixer.insertTextAfter(param, ': any');
+                },
+              });
+            }
+          });
+        }
+      },
+    };
+  },
+});
+
+module.exports = {
+  rules: {
+    'no-template-literals': rule,
+  },
+};
