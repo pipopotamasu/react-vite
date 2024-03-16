bb42187b8a6479957777ebac1a7263032e9e92c6
pipopotamasu <ec0156hx@gmail.com>
2024-03-16 15:07:23 -0400

    Update msw (#55)
    * update msw
    
    * migrate msw2
    
    * wip
    
    * upgrade msw

--- src/features/todos/routes/__tests__/Todos.test.tsx
+++ src/features/todos/routes/__tests__/Todos.test.tsx
@@ -1,16 +1,16 @@
 import { renderApp } from '@/test/testUtils';
 import { server } from '@/mocks/server';
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 describe('Todos', () => {
   it('renders', async () => {
     server.use(
-      rest.get('/todos', async (_, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.json({
+      http.get('/todos', async () => {
+        return HttpResponse.json(
+          {
             todos: [{ id: 1, title: 'todo11', done: false }],
-          })
+          },
+          { status: 200 }
         );
       })
     );
